2ac235f04038ca5113aee929d1b4953d6e98b910
Max Poshusta <MaxPoshusta@gmail.com>
2021-07-28 06:28:18 -0500

    Adding newline to typed message

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,6 +23,7 @@ export default {
       .pause(1000)
       .type('Developer. Tinkerer. Dad joke teller.')
       .pause(1500)
+      .break()
       .type('Learn more about me below')
       .go();
   },
