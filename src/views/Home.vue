262e9f6bbf681ab495a339dcfc6d6c04f8dd27b5
Max Poshusta <MaxPoshusta@gmail.com>
2021-08-02 06:56:07 -0500

    Adding period

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,7 +12,7 @@ export default {
   mounted() {
     // TODO: Max - is this plugin accessible? If not then make it so
     new TypeIt('#pageTitle')
-      .type('Professional bug writer')
+      .type('Professional bug writer.')
       .pause(500)
       .delete(23)
       .pause(1000)
