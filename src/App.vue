23ea991084177e793df644d5fdfa483e2561a86a
Max Poshusta <MaxPoshusta@gmail.com>
2021-08-26 06:41:38 -0500

    Adding temp nav link to experience

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/experience"></router-link>
+      <router-link to="/experience">Experience</router-link>
     </div>
     <router-view />
   </div>
