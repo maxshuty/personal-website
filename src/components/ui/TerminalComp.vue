02b74134ed9fbbe4490f9582aa18646e3d21cf05
Max Poshusta <MaxPoshusta@gmail.com>
2021-09-25 05:52:04 -0500

    Moving terminal to components

---
+++ src/components/ui/TerminalComp.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="terminal">Example</div>
+</template>
+
+<script>
+export default {
+  name: 'terminal-comp',
+};
+</script>
+
+<style lang="scss" scoped>
+.Terminal {
+  color: #ffffff;
+  background-color: #3a3b3c;
+  height: 100%;
+  width: 100%;
+}
+</style>
