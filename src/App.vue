47b27855803aa1e1cac69a9e80fe419a03e99358
Max Poshusta <MaxPoshusta@gmail.com>
2021-10-04 05:46:04 -0500

    Updating base route to terminal

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Terminal</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/experience">Experience</router-link>
     </div>
