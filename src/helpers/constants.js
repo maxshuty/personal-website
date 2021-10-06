5cc3e65c61165717ecf8c69833b05d3560b5f419
Max Poshusta <MaxPoshusta@gmail.com>
2021-10-06 06:16:37 -0500

    Adding help directory constant

---
+++ src/helpers/constants.js
@@ -0,0 +1,7 @@
+export const HelpDirectory = Object.freeze({
+  MK_WEBSITE: { command: 'mk website', description: 'creates a graphical user interface (GUI) of this website' },
+});
+
+export default {
+  HelpDirectory,
+};
