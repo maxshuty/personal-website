114bc65eeec4d81f6f15d7b3d5c11a50ee0cdff8
Max Poshusta <MaxPoshusta@gmail.com>
2021-10-07 06:43:24 -0500

    Adding options to help directory

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,6 +1,9 @@
 export const HelpDirectory = Object.freeze({
-  MK_WEBSITE: { command: 'mk website', description: 'creates a graphical user interface (GUI) of this website', options: ['']},
-
+  MK_WEBSITE: {
+    command: 'mk website',
+    description: 'creates a graphical user interface (GUI) of this website',
+    options: [{ prefix: '--go' }],
+  },
 });
 
 export default {
