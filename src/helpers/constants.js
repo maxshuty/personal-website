1017f15d7f012a7152713619e5e6df3e594bdec0
Max Poshusta <MaxPoshusta@gmail.com>
2021-10-07 06:44:36 -0500

    Renaming HelpDirectory to Commands for reusability

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,4 +1,4 @@
-export const HelpDirectory = Object.freeze({
+export const Commands = Object.freeze({
   MK_WEBSITE: {
     command: 'mk website',
     description: 'creates a graphical user interface (GUI) of this website',
@@ -7,5 +7,5 @@ export const HelpDirectory = Object.freeze({
 });
 
 export default {
-  HelpDirectory,
+  Commands,
 };
