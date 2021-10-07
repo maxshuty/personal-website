41a8d4570dccca02d524e126e82af82cb08b88ba
Max Poshusta <MaxPoshusta@gmail.com>
2021-10-07 06:47:25 -0500

    Updating MK_WEBSITE command description

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,7 +1,11 @@
 export const Commands = Object.freeze({
   MK_WEBSITE: {
     command: 'mk website',
-    description: 'creates a graphical user interface (GUI) of this website',
+    description: `
+    creates a graphical user interface (GUI) of this website. Use the
+    optional '--go' param with one of it's values to navigate directly
+    to any page.
+    `,
     options: [{ prefix: '--go', values: ['about', 'experience', 'home'] }],
   },
 });
