03e8e152e1a15f1752e31dfadeb5dba5a4848b90
Max Poshusta <MaxPoshusta@gmail.com>
2021-10-08 05:15:05 -0500

    Renaming command to expression like the real deal calls it

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -3,7 +3,7 @@ export const Commands = Object.freeze({
   // to do that, then in the values here use the same constant. That way when a new route is
   // added it will automatically be updated in this values list.
   MK_WEBSITE: {
-    command: 'mk website',
+    expression: 'mk website',
     description: `
     creates a graphical user interface (GUI) of this website. Use the
     optional '--go' param with one of it's values to navigate directly
