36130d0a443bd6dd88e7cbafbd2658c8578b694c
Max Poshusta <MaxPoshusta@gmail.com>
2021-10-13 06:03:54 -0500

    Adding about content

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
     <h1>Max Poshusta</h1>
+    <p>
+      Software develeper with many passions. Having spent many of my early years
+      simply not caring about accessibility until one day that I went to a park.
+    </p>
   </div>
 </template>
