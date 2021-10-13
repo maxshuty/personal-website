226e5bcdfd3a4d889294a5a853d66c7749f0cf7f
Max Poshusta <MaxPoshusta@gmail.com>
2021-10-13 06:04:09 -0500

    Adding about content

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,6 +4,8 @@
     <p>
       Software develeper with many passions. Having spent many of my early years
       simply not caring about accessibility until one day that I went to a park.
+      The park was completely accessible with wheel chair capable
+      merry-go-rounds to swings and zip-lines.
     </p>
   </div>
 </template>
