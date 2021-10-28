b24f83debac1d3def580de1a8541ace49fca4cf8
Max Poshusta <MaxPoshusta@gmail.com>
2021-10-28 06:30:13 -0500

    Removing nav styles from app.vue

--- src/App.vue
+++ src/App.vue
@@ -23,16 +23,5 @@ body {
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-  text-align: center;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
