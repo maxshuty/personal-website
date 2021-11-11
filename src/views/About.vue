9998a18ff75e5d5d201773944e3a5aee9d3f6505
Max Poshusta <MaxPoshusta@gmail.com>
2021-11-11 05:06:18 -0600

    Centering view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,28 +1,30 @@
 <template>
   <div class="about">
-    <h1>Max Poshusta</h1>
-    <p>Born in 1989 in Rochester Minnesota</p>
-
-    <div>
-      <h2>My journey into Computers</h2>
+    <div class="about__content">
+      <h1>Max Poshusta</h1>
+      <p>Born in 1989 in Rochester Minnesota</p>
 
       <div>
-        <h3>Accessibility story</h3>
-        <p>
-          Software develeper with many passions. Having spent many of my early
-          years simply not caring about accessibility until one day that I went
-          to a park. The park was completely accessible with wheel chair capable
-          merry-go-rounds to swings and zip-lines. I had an epiphany - not a
-          single application I had ever worked on was accessible. I was
-          literally preventing people from using applications I had built due to
-          my own ignorance.
-        </p>
-        <p>
-          After this realization I took it upon myself to change my ways... To
-          be an advocate for accessibility. To help advocate for other
-          developers to make their applications accessible both at work and
-          through contributions online.
-        </p>
+        <h2>My journey into Computers</h2>
+
+        <div>
+          <h3>Accessibility story</h3>
+          <p>
+            Software develeper with many passions. Having spent many of my early
+            years simply not caring about accessibility until one day that I
+            went to a park. The park was completely accessible with wheel chair
+            capable merry-go-rounds to swings and zip-lines. I had an epiphany -
+            not a single application I had ever worked on was accessible. I was
+            literally preventing people from using applications I had built due
+            to my own ignorance.
+          </p>
+          <p>
+            After this realization I took it upon myself to change my ways... To
+            be an advocate for accessibility. To help advocate for other
+            developers to make their applications accessible both at work and
+            through contributions online.
+          </p>
+        </div>
       </div>
     </div>
   </div>
