3758c52b64546d20e6c7fc511053e47cc5962906
Max Poshusta <MaxPoshusta@gmail.com>
2021-11-18 06:27:11 -0600

    Adding musical tastes list

--- src/views/About.vue
+++ src/views/About.vue
@@ -44,7 +44,7 @@
           <li>Dirty Heads - "Cabin by the sea"</li>
           <li>Matisyahu - "Live like a warrior"</li>
           <li>Ben Folds - "Still fighting it"</li>
-          <li>Coun</li>
+          <li>Counting Crows - "Mr. Jones"</li>
           <li>Spin Doctors - "Two Princes"</li>
         </ul>
       </div>
