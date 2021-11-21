4b6c1c969674cfd58f72fc56251a5e296ba576ab
Max Poshusta <MaxPoshusta@gmail.com>
2021-11-21 06:16:07 -0600

    Adding musical tastes list

--- src/views/About.vue
+++ src/views/About.vue
@@ -47,7 +47,7 @@
           <li>Counting Crows - "Mr. Jones"</li>
           <li>Spin Doctors - "Two Princes"</li>
           <li>Falling in Reverse - "Coming Home"</li>
-          <li>Bob Ma</li>
+          <li>Bob Mar</li>
           <li>Antix - "Hands up 2"</li>
         </ul>
       </div>
