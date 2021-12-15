1ef77f1c28a3b4d528d18cd75d85496b25876740
Max Poshusta <MaxPoshusta@gmail.com>
2021-12-15 08:39:32 -0600

    Adding musical tastes list

--- src/views/About.vue
+++ src/views/About.vue
@@ -49,7 +49,7 @@
           <li>Falling in Reverse - "Coming Home"</li>
           <li>Bob Marley - "Redemption Song"</li>
           <li>Antix - "Hands up 2"</li>
-          <li>Queen - "Bohemian R</li>
+          <li>Queen - "Bohemian Rhapsody"</li>
         </ul>
       </div>
     </div>
