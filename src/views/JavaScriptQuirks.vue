e0901f8f34bb0ed4fd974dedbc6c8a14a24549c6
Max Poshusta <MaxPoshusta@gmail.com>
2022-02-11 06:08:30 -0600

    Adding JS Quirks

--- src/views/JavaScriptQuirks.vue
+++ src/views/JavaScriptQuirks.vue
@@ -6,10 +6,7 @@
       ((89.99999999999999 !== 90.00000000000000) !== (89.999999999999999 !==
       90.000000000000000)) // true. Something something floats suck
     </code>
-
-    <code>
-      function whatWillIReturn() { try { return 'foo'; } catch(up) { throw(up);
-      } finally { return 'bar'; } console.log(whatWillIReturn()); // 'bar'.
+    <p>
       Finally always executes, so the first return is thrown away
     </code>
 
